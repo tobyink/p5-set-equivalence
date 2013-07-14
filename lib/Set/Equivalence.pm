9bce5bd12f41e7cb0dfd0e15026a310803d83878
Toby Inkster <mail@tobyinkster.co.uk>
2013-07-14 21:26:12 +0100

    prepare for release

--- lib/Set/Equivalence.pm
+++ lib/Set/Equivalence.pm
@@ -6,7 +6,7 @@ use warnings;
 
 BEGIN {
 	$Set::Equivalence::AUTHORITY = 'cpan:TOBYINK';
-	$Set::Equivalence::VERSION   = '0.001';
+	$Set::Equivalence::VERSION   = '0.000_01';
 }
 
 use Carp qw( croak );
